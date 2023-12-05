87cb92680863dc155da3cebf9b9b003724195028
Giulio Marciante <87944393+undeermind@users.noreply.github.com>
2023-12-05 19:58:55 +0100

    Esercizio Testing4

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.time.OffsetDateTime;
+
+//Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
+//ottieni l'anno
+//ottieni il mese
+//ottieni il giorno
+//ottieni il giorno della settimana
+//Stampa i risultati sulla console
+public class Main {
+    public static void main(String[] args) {
+
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        getYear(date);
+        getMonth(date);
+        getDay(date);
+        getDayOfWeek(date);
+    }
+    public static int getYear(OffsetDateTime date){
+        int dataYear = date.getYear();
+        System.out.println("Anno: " + dataYear);
+        return dataYear;
+    }
+    public static Month getMonth(OffsetDateTime date){
+        Month dataMonth = date.getMonth();
+        System.out.println("Mese: " + dataMonth);
+        return dataMonth;
+    }
+    public static int getDay(OffsetDateTime date){
+        int dataDay = date.getDayOfMonth();
+        System.out.println("Giorno: " + dataDay);
+        return dataDay;
+    }
+    public static DayOfWeek getDayOfWeek(OffsetDateTime date){
+        DayOfWeek dataDayOfWeek = date.getDayOfWeek();
+        System.out.println("Giorno della settimana: " + dataDayOfWeek);
+        return dataDayOfWeek;
+    }
+}
---
+++ src/MainTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.time.OffsetDateTime;
+
+import static java.time.DayOfWeek.WEDNESDAY;
+import static java.time.Month.MARCH;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class MainTest {
+
+    private final Main testing = new Main();
+    OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+    @Test
+    void getYearTest () {
+        int result = testing.getYear(date);
+        assertEquals(2023,result);
+    }
+    @Test
+    void getMonthTest() {
+        Month result = testing.getMonth(date);
+        assertEquals(MARCH,result);
+    }
+
+    @Test
+    void getDayTest() {
+        int result = testing.getDay(date);
+        assertEquals(1,result);
+    }
+
+    @Test
+    void getDayOfWeekTest() {
+        DayOfWeek result = testing.getDayOfWeek(date);
+        assertEquals(WEDNESDAY,result);
+    }
+}
